07cb13fb316837a487ab33ec180469ce1617d1e9
hdev05 <hamzah.dev.061223@gmail.com>
2024-12-22 14:02:38 +0800

    merincikan perolehan medali

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -21,15 +21,98 @@ class LaporanController extends Controller
         $remaja = $medalis->where('golongan', 'REMAJA')->count();
         $dewasa = $medalis->where('golongan', 'DEWASA')->count();
 
+        // rincian jenis medali
+        $usia_dini_1 = $medalis->where('golongan', 'USIA DINI')->where('medali', 1)->count();
+        $usia_dini_2 = $medalis->where('golongan', 'USIA DINI')->where('medali', 2)->count();
+        $usia_dini_3 = $medalis->where('golongan', 'USIA DINI')->where('medali', 3)->count();
+        $pra_remaja_1 = $medalis->where('golongan', 'PRA REMAJA')->where('medali', 1)->count();
+        $pra_remaja_2 = $medalis->where('golongan', 'PRA REMAJA')->where('medali', 2)->count();
+        $pra_remaja_3 = $medalis->where('golongan', 'PRA REMAJA')->where('medali', 3)->count();
+        $remaja_1 = $medalis->where('golongan', 'REMAJA')->where('medali', 1)->count();
+        $remaja_2 = $medalis->where('golongan', 'REMAJA')->where('medali', 2)->count();
+        $remaja_3 = $medalis->where('golongan', 'REMAJA')->where('medali', 3)->count();
+        $dewasa_1 = $medalis->where('golongan', 'DEWASA')->where('medali', 1)->count();
+        $dewasa_2 = $medalis->where('golongan', 'DEWASA')->where('medali', 2)->count();
+        $dewasa_3 = $medalis->where('golongan', 'DEWASA')->where('medali', 3)->count();
+
+        // perolehan L
+        $usia_dini_l_1 = $medalis->where('golongan', 'USIA DINI')->where('jk', 'L')->where('medali', 1)->count();
+        $usia_dini_l_2 = $medalis->where('golongan', 'USIA DINI')->where('jk', 'L')->where('medali', 2)->count();
+        $usia_dini_l_3 = $medalis->where('golongan', 'USIA DINI')->where('jk', 'L')->where('medali', 3)->count();
+        $pra_remaja_l_1 = $medalis->where('golongan', 'PRA REMAJA')->where('jk', 'L')->where('medali', 1)->count();
+        $pra_remaja_l_2 = $medalis->where('golongan', 'PRA REMAJA')->where('jk', 'L')->where('medali', 2)->count();
+        $pra_remaja_l_3 = $medalis->where('golongan', 'PRA REMAJA')->where('jk', 'L')->where('medali', 3)->count();
+        $remaja_l_1 = $medalis->where('golongan', 'REMAJA')->where('jk', 'L')->where('medali', 1)->count();
+        $remaja_l_2 = $medalis->where('golongan', 'REMAJA')->where('jk', 'L')->where('medali', 2)->count();
+        $remaja_l_3 = $medalis->where('golongan', 'REMAJA')->where('jk', 'L')->where('medali', 3)->count();
+        $dewasa_l_1 = $medalis->where('golongan', 'DEWASA')->where('jk', 'L')->where('medali', 1)->count();
+        $dewasa_l_2 = $medalis->where('golongan', 'DEWASA')->where('jk', 'L')->where('medali', 2)->count();
+        $dewasa_l_3 = $medalis->where('golongan', 'DEWASA')->where('jk', 'L')->where('medali', 3)->count();
+
+        // perolehan P
+        $usia_dini_p_1 = $medalis->where('golongan', 'USIA DINI')->where('jk', 'P')->where('medali', 1)->count();
+        $usia_dini_p_2 = $medalis->where('golongan', 'USIA DINI')->where('jk', 'P')->where('medali', 2)->count();
+        $usia_dini_p_3 = $medalis->where('golongan', 'USIA DINI')->where('jk', 'P')->where('medali', 3)->count();
+        $pra_remaja_p_1 = $medalis->where('golongan', 'PRA REMAJA')->where('jk', 'P')->where('medali', 1)->count();
+        $pra_remaja_p_2 = $medalis->where('golongan', 'PRA REMAJA')->where('jk', 'P')->where('medali', 2)->count();
+        $pra_remaja_p_3 = $medalis->where('golongan', 'PRA REMAJA')->where('jk', 'P')->where('medali', 3)->count();
+        $remaja_p_1 = $medalis->where('golongan', 'REMAJA')->where('jk', 'P')->where('medali', 1)->count();
+        $remaja_p_2 = $medalis->where('golongan', 'REMAJA')->where('jk', 'P')->where('medali', 2)->count();
+        $remaja_p_3 = $medalis->where('golongan', 'REMAJA')->where('jk', 'P')->where('medali', 3)->count();
+        $dewasa_p_1 = $medalis->where('golongan', 'DEWASA')->where('jk', 'P')->where('medali', 1)->count();
+        $dewasa_p_2 = $medalis->where('golongan', 'DEWASA')->where('jk', 'P')->where('medali', 2)->count();
+        $dewasa_p_3 = $medalis->where('golongan', 'DEWASA')->where('jk', 'P')->where('medali', 3)->count();
+
+        // 
+
         return view('laporan.perolehan-medali', compact(
-            'medalis', 
-            'emas', 
-            'perak', 
+            'medalis',
+            'emas',
+            'perak',
             'perunggu',
             'usia_dini',
             'pra_remaja',
             'remaja',
             'dewasa',
+
+            'usia_dini_1',
+            'usia_dini_2',
+            'usia_dini_3',
+            'pra_remaja_1',
+            'pra_remaja_2',
+            'pra_remaja_3',
+            'remaja_1',
+            'remaja_2',
+            'remaja_3',
+            'dewasa_1',
+            'dewasa_2',
+            'dewasa_3',
+
+            'usia_dini_l_1',
+            'usia_dini_l_2',
+            'usia_dini_l_3',
+            'pra_remaja_l_1',
+            'pra_remaja_l_2',
+            'pra_remaja_l_3',
+            'remaja_l_1',
+            'remaja_l_2',
+            'remaja_l_3',
+            'dewasa_l_1',
+            'dewasa_l_2',
+            'dewasa_l_3',
+
+            'usia_dini_p_1',
+            'usia_dini_p_2',
+            'usia_dini_p_3',
+            'pra_remaja_p_1',
+            'pra_remaja_p_2',
+            'pra_remaja_p_3',
+            'remaja_p_1',
+            'remaja_p_2',
+            'remaja_p_3',
+            'dewasa_p_1',
+            'dewasa_p_2',
+            'dewasa_p_3',
         ));
     }
 }
--- resources/views/laporan/perolehan-medali.blade.php
+++ resources/views/laporan/perolehan-medali.blade.php
@@ -39,32 +39,132 @@
 
         <section class="mb-4 card p-3">
             <h4>Perolehan Medali Berdasarkan Golongan:</h4>
-                <div class="row row-cols-2 row-cols-md-4 g-2">
-                    <div class="col">
-                        <div class="card p-2 text-bg-primary text-center">
-                            <h4>USIA DINI :</h2>
-                                <h3>{{ $usia_dini }}</h3>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card p-2 text-bg-primary text-center">
-                            <h4>PRA REMAJA :</h2>
-                                <h3>{{ $pra_remaja }}</h3>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card p-2 text-center text-white text-bg-danger">
-                            <h4>REMAJA :</h2>
-                                <h3>{{ $remaja }}</h3>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card p-2 text-center text-white text-bg-danger">
-                            <h4>DEWASA :</h2>
-                                <h3>{{ $dewasa }}</h3>
-                        </div>
+            <div class="row row-cols-1 row-cols-md-4 g-2">
+                <div class="col">
+                    <div class="card p-2 text-bg-primary text-center">
+                        <h4>USIA DINI : {{ $usia_dini }}</h2>
+                            <hr>
+                            <div class="d-flex justify-content-center">
+                                <span class="btn btn-sm btn-outline-light">Em : {{ $usia_dini_1 }}</span>
+                                <span class="mx-2 btn btn-sm btn-outline-light">Pr : {{ $usia_dini_2 }}</span>
+                                <span class="btn btn-sm btn-outline-light">Pg : {{ $usia_dini_3 }}</span>
+                            </div>
+                            <hr>
+                            <div class="d-flex">
+                                <div>
+                                    <h5>PA</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $usia_dini_l_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $usia_dini_l_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $usia_dini_l_3 }}</span>
+                                    </div>
+                                </div>
+                                <span>|</span>
+                                <div>
+                                    <h5>PI</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $usia_dini_p_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $usia_dini_p_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $usia_dini_p_3 }}</span>
+                                    </div>
+                                </div>
+                            </div>
                     </div>
                 </div>
+                <div class="col">
+                    <div class="card p-2 text-bg-primary text-center">
+                        <h4>PRA REMAJA : {{ $pra_remaja }}</h2>
+                            <hr>
+                            <div class="d-flex justify-content-center">
+                                <span class="btn btn-sm btn-outline-light">Em : {{ $pra_remaja_1 }}</span>
+                                <span class="mx-2 btn btn-sm btn-outline-light">Pr : {{ $pra_remaja_2 }}</span>
+                                <span class="btn btn-sm btn-outline-light">Pg : {{ $pra_remaja_3 }}</span>
+                            </div>
+                            <hr>
+                            <div class="d-flex">
+                                <div>
+                                    <h5>PA</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $pra_remaja_l_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $pra_remaja_l_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $pra_remaja_l_3 }}</span>
+                                    </div>
+                                </div>
+                                <span>|</span>
+                                <div>
+                                    <h5>PI</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $pra_remaja_p_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $pra_remaja_p_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $pra_remaja_p_3 }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card p-2 text-center text-white text-bg-danger">
+                        <h4>REMAJA : {{ $remaja }}</h2>
+                            <hr>
+                            <div class="d-flex justify-content-center">
+                                <span class="btn btn-sm btn-outline-light">Em : {{ $remaja_1 }}</span>
+                                <span class="mx-2 btn btn-sm btn-outline-light">Pr : {{ $remaja_2 }}</span>
+                                <span class="btn btn-sm btn-outline-light">Pg : {{ $remaja_3 }}</span>
+                            </div>
+                            <hr>
+                            <div class="d-flex">
+                                <div>
+                                    <h5>PA</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $remaja_l_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $remaja_l_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $remaja_l_3 }}</span>
+                                    </div>
+                                </div>
+                                <span>|</span>
+                                <div>
+                                    <h5>PI</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $remaja_p_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $remaja_p_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $remaja_p_3 }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card p-2 text-center text-white text-bg-danger">
+                        <h4>DEWASA : {{ $dewasa }}</h2>
+                            <hr>
+                            <div class="d-flex justify-content-center">
+                                <span class="btn btn-sm btn-outline-light">Em : {{ $dewasa_1 }}</span>
+                                <span class="mx-2 btn btn-sm btn-outline-light">Pr : {{ $dewasa_2 }}</span>
+                                <span class="btn btn-sm btn-outline-light">Pg : {{ $dewasa_3 }}</span>
+                            </div>
+                            <hr>
+                            <div class="d-flex">
+                                <div>
+                                    <h5>PA</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $dewasa_l_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $dewasa_l_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $dewasa_l_3 }}</span>
+                                    </div>
+                                </div>
+                                <span>|</span>
+                                <div>
+                                    <h5>PI</h5>
+                                    <div class="d-flex justify-content-center">
+                                        <span class="btn btn-sm btn-outline-light">Em: {{ $dewasa_p_1 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pr: {{ $dewasa_p_2 }}</span>
+                                        <span class="btn btn-sm btn-outline-light">Pg: {{ $dewasa_p_3 }}</span>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </section>
 
         <section>
